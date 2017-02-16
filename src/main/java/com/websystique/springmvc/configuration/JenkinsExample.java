c08ff595c031133dba569119513003ec2f416053
Raya <rayameha@gmail.com>
2017-02-16 16:33:13 -0500

    Initial commit

---
+++ src/main/java/com/websystique/springmvc/configuration/JenkinsExample.java
@@ -0,0 +1,10 @@
+package com.websystique.springmvc.configuration;
+
+public class JenkinsExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("i am working on Jenkins authomatic build!");
+	}
+
+}
