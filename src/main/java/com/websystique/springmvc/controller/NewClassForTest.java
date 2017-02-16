4f96604a64efb6867a5b28d78e3a53f4f8afb13e
Raya <rayameha@gmail.com>
2017-02-16 14:56:18 -0500

    my second update

---
+++ src/main/java/com/websystique/springmvc/controller/NewClassForTest.java
@@ -0,0 +1,10 @@
+package com.websystique.springmvc.controller;
+
+public class NewClassForTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("It does not do anything, just working around!");
+	}
+
+}
